a32b27356b405cd579cd4037f3eafe322202f7f2
calebbillman <cbillman@thermotron.com>
2018-03-15 14:42:01 -0400

    Wrong slash
    Linux doesn't like "/" in files...

--- include/fluent/named_type.hpp
+++ include/fluent/named_type.hpp
@@ -1,7 +1,7 @@
 #ifndef NAMED_TYPE_HPP
 #define NAMED_TYPE_HPP
 
-#include <fluent\named_type_impl.hpp>
-#include <fluent\underlying_functionalities.hpp>
+#include <fluent/named_type_impl.hpp>
+#include <fluent/underlying_functionalities.hpp>
 
 #endif
